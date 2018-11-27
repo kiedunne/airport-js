8957ace73ada6d9ac43f2c0bb5217e802a6da7d3
leoncross <leon@lscross.co.uk>
2018-11-27 15:29:21 +0000

    silliness with randomizing

---
+++ src/weather.js
@@ -0,0 +1,20 @@
+function Weather() {
+  this.randweather = ['Sunny', 'Stormy']
+
+}
+
+
+
+Weather.prototype.randomize = function () {
+  return 1
+
+  // var nweather = this.randweather[Math.floor(Math.random()*this.randweather.length)]
+  // return nweather
+};
+//
+// weather = new Weather()
+// console.log(weather)
+
+
+var nweather = Math.random
+)
